098e174f4026796b060eeb276d564a8c3829f66b
Tyler McKee <tmckeevt@gmail.com>
2024-12-16 11:12:29 -0500

    remove directory voodoo

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,7 +5,6 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
-	"path/filepath"
 
 	"go.tsmckee.com/cmd/models"
 )
@@ -17,19 +16,11 @@ type Application struct {
 
 func main() {
 	g := models.CreateGarden()
-	defaultWD, _ := os.Getwd()
-	home, err := os.UserHomeDir()
-	if err != nil {
-		panic(err)
-	}
-	home = filepath.Join(home, "prg/tsm/")
-	os.Chdir(home)
+
 	// TODO make content dir in config or something to search files in
 	// for now Im just going to hack in static
 	g.PopulateGardenFromDir("ui/content")
 	g.ParseAllConnections()
-
-	os.Chdir(defaultWD)
 
 	addr := flag.String("addr", "localhost:3000", "HTTP Network Address")
 	flag.Parse()
@@ -41,7 +32,7 @@ func main() {
 		garden: g,
 	}
 	logger.Info("Starting server on", "addr", *addr)
-	err = http.ListenAndServe(*addr, app.routes())
+	err := http.ListenAndServe(*addr, app.routes())
 
 	logger.Error(err.Error())
 	os.Exit(1)
