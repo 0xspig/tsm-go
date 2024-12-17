2148bbf56c02c73419b3d5c04018a29944fd293c
Tyler <tmckeevt@gmail.com>
2024-12-17 14:51:05 -0500

    changed some display

--- ui/src/main.js
+++ ui/src/main.js
@@ -4,11 +4,34 @@ const Graph = new ForceGraph3D(document.getElementById('3d-graph'));
 var xmlhttp = new XMLHttpRequest;
 var parsedJson;
 
+Graph.backgroundColor("#0d1e1f");
+
 
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
 		console.log(this.responseText);
 		Graph.graphData(JSON.parse(this.responseText));
+    Graph.nodeColor(node => {
+      console.log(node);
+      //md files
+      if (node.data_type == 1){
+        return "#fff380";
+      }
+      // tags
+      if (node.data_type == 2){
+        return "#6b93c6";
+      }
+    });
+    Graph.nodeVal(node => {
+      //md files
+      if (node.data_type == 1){
+        return 1;
+      }
+      // tags
+      if (node.data_type == 2){
+        return 3;
+      }
+    })
     }
 };
 xmlhttp.open("GET", "/graph-json", true);
