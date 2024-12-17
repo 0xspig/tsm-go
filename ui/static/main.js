5225f289cb49cb07301c66ba00791bc0d204ab6a
Tyler McKee <tmckeevt@gmail.com>
2024-12-17 14:51:36 -0500

    changed some display

--- ui/static/main.js
+++ ui/static/main.js
@@ -89599,11 +89599,34 @@ function getGraphObj(object) {
 const Graph = new _3dForceGraph(document.getElementById('3d-graph'));
 var xmlhttp = new XMLHttpRequest;
 
+Graph.backgroundColor("#0d1e1f");
+
 
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
 		console.log(this.responseText);
 		Graph.graphData(JSON.parse(this.responseText));
+    Graph.nodeColor(node => {
+      console.log(node);
+      //md files
+      if (node.data_type == 1){
+        return "#fff380";
+      }
+      // tags
+      if (node.data_type == 2){
+        return "#6b93c6";
+      }
+    });
+    Graph.nodeVal(node => {
+      //md files
+      if (node.data_type == 1){
+        return 1;
+      }
+      // tags
+      if (node.data_type == 2){
+        return 3;
+      }
+    });
     }
 };
 xmlhttp.open("GET", "/graph-json", true);
