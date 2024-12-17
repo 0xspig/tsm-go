87f26f29ba8888c15251de8468af0bf9c60242ae
Tyler McKee <tmckeevt@gmail.com>
2024-12-17 12:10:06 -0500

    remove test graph

--- ui/static/main.js
+++ ui/static/main.js
@@ -1,15 +1,4 @@
 import "./3d-force-graph.min.js"
- // Random tree
- const N = 300;
- const gData = {
-   nodes: [...Array(N).keys()].map(i => ({ id: i })),
-   links: [...Array(N).keys()]
-	 .filter(id => id)
-	 .map(id => ({
-	   source: id,
-	   target: Math.round(Math.random() * (id-1))
-	 }))
- };
 
 const Graph = new ForceGraph3D(document.getElementById('3d-graph'));
 var xmlhttp = new XMLHttpRequest;
