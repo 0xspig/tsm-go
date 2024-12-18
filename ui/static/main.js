7f31f1b270bba6e24078b1b3479b90a62cd39862
Tyler McKee <tmckeevt@gmail.com>
2024-12-17 19:43:58 -0500

    snap on click

--- ui/static/main.js
+++ ui/static/main.js
@@ -89601,6 +89601,21 @@ var xmlhttp = new XMLHttpRequest;
 
 Graph.backgroundColor("#0d1e1f");
 
+Graph.onNodeClick(node => {
+  // Aim at node from outside it
+  const distance = 600;
+  const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
+
+  const newPos = node.x || node.y || node.z
+    ? { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }
+    : { x: 0, y: 0, z: distance }; // special case if node is in (0,0,0)
+
+  Graph.cameraPosition(
+    newPos, // new position
+    node, // lookAt ({ x, y, z })
+    1800  // ms transition duration
+  );
+});
 
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
